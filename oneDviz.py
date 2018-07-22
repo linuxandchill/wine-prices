af07065bb24d296c2da95c5296dda25c963f1dcf
linuxandchill <rivercodi@gmail.com>
2018-07-22 14:35:26 -0700

    model.add

--- oneDviz.py
+++ oneDviz.py
@@ -2,7 +2,8 @@ import keras
 from keras import layers
 from keras.datasets import imdb
 from keras.preprocessing import sequence
-from keras.layers import Dense, Embedding
+from keras.layers import Dense, Embedding, Conv1D 
+from keras.layers import GlobalMaxPooling1D, MaxPooling1D
 
 max_features = 2000
 max_len = 500
@@ -11,6 +12,7 @@ max_len = 500
 x_train = sequence.pad_sequences(x_train, maxlen=max_len)
 x_test = sequence.pad_sequences(x_test, maxlen=max_len)
 
+'''
 model = keras.models.Sequential([
     Embedding(max_features, 128,
         input_length=max_len,
@@ -31,7 +33,6 @@ model.add(layers.MaxPooling1D(5))
 model.add(layers.Conv1D(32,7,activation='relu'))
 model.add(layers.GlobalMaxPooling1D())
 model.add(layers.Dense(1))
-'''
 model.summary()
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
